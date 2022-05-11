82774e2b7f463e1c354322a1c80ae371671a7af5
muqhc <muqhc07@gmail.com>
2022-05-11 14:27:10 +0900

    test update

--- kotching-test/src/main/kotlin/io/github/muqhc/kotching/test/Playground1.kt
+++ kotching-test/src/main/kotlin/io/github/muqhc/kotching/test/Playground1.kt
@@ -1,5 +1,22 @@
 package io.github.muqhc.kotching.test
 
-fun main() {
+import io.github.muqhc.kotching.*
+import io.github.muqhc.kotching.dsl.*
 
+val strangeColor = function(RGB) { r,g,b ->
+    Color(
+        (255-r)*256*256+
+                (255-g)*256+
+                (255-b)
+    )
+}
+
+val printColor = function(RGB) { r,g,b ->
+    println("Color(red=$r,green=$g,blue=$b)")
+}
+
+fun main() {
+    val magenta = Color(0xFF00FF)
+    printColor(magenta)
+    printColor(strangeColor(magenta) as Color)
 }
